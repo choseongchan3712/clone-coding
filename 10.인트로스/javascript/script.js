1d0d91bcb33f2ff467ef82f30c04638d9aabea40
choseongchan3712 <choseongchan3712@naver.com>
2024-09-15 23:18:24 +0900

    헤더완성

--- 10.인트로스/javascript/script.js
+++ 10.인트로스/javascript/script.js
@@ -1,21 +1,63 @@
-const header = document.querySelector("header");
-const menuWrap = document.querySelector(".menu_wrap");
-const headerWhiteBox = document.querySelector(".white_box");
-const menuText = document.querySelectorAll(".menu_text");
-const logoWhite = document.querySelector(".logo_white");
-const logoBlack = document.querySelector(".logo_black");
+const menuContainer = document.querySelector(".menu_container");
+const whiteBg = document.querySelector(".white_bg");
+const blackLine = document.querySelector(".black_line");
+const blackLogo = document.querySelector(".black_logo");
+const whiteLogo = document.querySelector(".white_logo");
+const languageImg = document.querySelector(".language>img");
+const sideMenuImg = document.querySelector(".side_menu>img");
+const sideText = document.querySelectorAll(".side_text > li");
+const closeBtn = document.querySelector(".close_btn");
+const sideSlide = document.querySelector(".side_menu_click")
+const sideMenu = document.querySelector(".side_menu");
 
-menuText.forEach((i) => {
-  i.addEventListener("mouseover", () => {
-    headerWhiteBox.style.display = "block";
-    logoWhite.style.opacity = "0";
-    logoBlack.style.display = "block";
+menuContainer.addEventListener("mouseover", () => {
+  whiteBg.style.display = "block";
+  blackLine.style.display = "block";
+  whiteLogo.style.opacity = "0";
+  blackLogo.style.display = "block";
+  languageImg.style.filter = "invert(100%)";
+  sideMenuImg.style.filter = "invert(100%)";
+});
+menuContainer.addEventListener("mouseleave", () => {
+  whiteBg.style.display = "none";
+  blackLine.style.display = "none";
+  whiteLogo.style.opacity = "1";
+  blackLogo.style.display = "none";
+  languageImg.style.filter = "invert(0)";
+  sideMenuImg.style.filter = "invert(0)";
+});
+
+sideText.forEach((n) => {
+  let boldtext = n.querySelector(".side_bold p");
+  boldtext.addEventListener("mouseover", () => {
+    let otherBold = document.querySelectorAll(".side_bold p");
+    otherBold.forEach((o) => {
+      o.style.opacity = "0.5";
+    });
+    boldtext.style.opacity = "1";
+
+    let sideLine = n.querySelector(".side_line");
+    let otherLine = document.querySelectorAll(".side_line");
+    otherLine.forEach((o) => {
+      o.style.opacity = "0";
+    });
+    sideLine.style.opacity = "1";
+
+    let reText = n.querySelector(".side_re");
+    let otherRe = document.querySelectorAll(".side_re");
+    otherRe.forEach((o) => {
+      o.style.display = "none";
+    });
+    reText.style.display = "flex";
   });
 });
-//# 노드 선택을 querySelectorAll로 했다면, 이벤트를 부여할때 forEach를 사용해야 한다.
 
-menuWrap.addEventListener("mouseleave", () => {
-  headerWhiteBox.style.display = "none";
-  logoWhite.style.opacity = "1";
-  logoBlack.style.display = "none";
-});
+closeBtn.addEventListener("click", () => {
+  sideSlide.style.transform = "translateX(100vw)";
+  sideSlide.style.opacity = "0";
+})
+
+sideMenu.addEventListener("click", () => {
+  sideSlide.style.transform = "translateX(0)";
+  sideSlide.style.opacity = "1";
+})
